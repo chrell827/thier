3b5a4dc4a169e1852e94b6ce6054e0112945485d
Christian <chrell827@gmail.com>
2020-02-15 16:55:31 -0500

    fully working

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 
+var score = 0;
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  
   state = {
     friendsList: friends,
     clicked: [],
@@ -13,36 +14,45 @@ class App extends Component {
   };
 
   clickCharacter = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    //const friends = this.state.friendsList.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    
-    //this.state.friendsList.forEach(friend => {});
-    
-    
-    const newArr = this.state.friendsList.slice();
-    const shuffleArr = [];
-    while (newArr.length > 0) {
-      shuffleArr.push(newArr.splice(Math.floor(Math.random() * newArr.length), 1)[0]);
+    if(this.state.clicked.includes(id))
+    {
+      alert("You LOSE!");
+      score = 0;
+      this.setState({friendsList: friends, clicked: [], score: 0});
     }
-
-    this.setState({ friendsList: shuffleArr });
+    else{
+      score++;
+      let newClicked = this.state.clicked.slice();
+      newClicked.push(id);
+      const newArr = this.state.friendsList.slice();
+      const shuffleArr = [];
+      while (newArr.length > 0) {
+        shuffleArr.push(newArr.splice(Math.floor(Math.random() * newArr.length), 1)[0]);
+      }
+      if(score === 12){
+        score = 0;
+        alert("You WIN!");
+        console.log("SCORE: " + this.state.score);
+        this.setState({friendsList: friends, clicked: [], score: score}); 
+      }
+      else{
+      this.setState({ friendsList: shuffleArr, clicked: newClicked, score: this.state.score+1});
+      }
+      
+    }
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Clicky Game</Title>
+        <Title>Score: {this.state.score}</Title>
         {this.state.friendsList.map(friend => (
           <FriendCard
             clickCharacter={this.clickCharacter}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
